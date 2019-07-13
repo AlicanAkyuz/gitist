adabf494f6b838669f481262db00f3048336044f
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-07-13 13:24:04 +0300

    apollo configs

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,21 @@ import React, { FunctionComponent } from 'react';
 import ReactDOM from 'react-dom';
 import { setConfig } from 'react-hot-loader';
 import { BrowserRouter } from 'react-router-dom';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
 
 import App from './App';
 
-const uri = 'https://data.github.ist/graphiql';
-const client = new ApolloClient({ uri });
+const client = new ApolloClient({
+  ssrForceFetchDelay: 50,
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'https://data.github.ist/graphiql',
+    fetch,
+  }),
+});
 
 setConfig({
   ignoreSFC: true,
